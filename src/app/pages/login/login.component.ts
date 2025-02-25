fecddb75fb2761ad550ce3a3400880f972c496ac
Wellington Carvalho <88244614+aguiarwellington@users.noreply.github.com>
2025-02-25 12:15:59 -0300

    formulario de email e senha

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from "../../components/default-login-layout/default-login-layout.component";
 import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
+import {PrimaryInputComponent} from '../../components/primary-input/primary-input.component';
 
 interface LoginForm{
   email: string;
@@ -11,8 +12,10 @@ interface LoginForm{
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [DefaultLoginLayoutComponent,
-    ReactiveFormsModule
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
